cc10dab743b622e3e731d1e9eead415f0fbb2078
ana-mihaela <ana.mihaela.v@gmail.com>
2018-03-19 16:35:37 +0200

    -

--- src/app/feature/ran/site/site.routes.ts
+++ src/app/feature/ran/site/site.routes.ts
@@ -1,7 +1,10 @@
 import { SiteListComponent } from './site-list/site-list.component';
+import { SiteItemComponent } from './site-item/site-item.component';
 
 export const routes = [
   { path: '', children: [
-    { path: '', component: SiteListComponent }
+    { path: '', component: SiteListComponent },
+    { path: 'site/:id', component: SiteItemComponent },
+    { path: 'site/add', component: SiteItemComponent },
   ]},
 ];
